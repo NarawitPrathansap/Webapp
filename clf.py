c80d56615b5061fcc295c6b9f74c624e4ee98ae4
NarawitPrathansap <narawit.p@kkumail.com>
2024-03-06 22:19:36 +0700

    add

--- clf.py
+++ clf.py
@@ -6,11 +6,11 @@ import torch
 
 
 def classify_question(text, tokenizer, bert_model, random_forest_model):
-    inputs = tokenizer(text, return_tensors="tf", max_length=512, truncation=True, padding='max_length')
-    output = bert_model(inputs)
+    inputs = tokenizer(text, return_tensors="pt", max_length=512, truncation=True, padding='max_length')
+    output = bert_model(**inputs)
     last_hidden_states = output.last_hidden_state
-    cls_embeddings = last_hidden_states[:, 0, :]
-    predictions = random_forest_model.predict(cls_embeddings.numpy())
+    cls_embeddings = last_hidden_states[:, 0, :].detach().numpy()
+    predictions = random_forest_model.predict(cls_embeddings)
     return predictions[0]
 
 if __name__ == '__main__':

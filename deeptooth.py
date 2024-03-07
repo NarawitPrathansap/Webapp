17cef2ae4acdd05faee9d120abefb60d46e84561
NarawitPrathansap <narawit.p@kkumail.com>
2024-03-07 16:54:49 +0700

    add

--- deeptooth.py
+++ deeptooth.py
@@ -557,40 +557,40 @@ def predict():
             elif prediction_class == 3:
                 shap_values = explainer15_23_age.shap_values(reshaped_user_uploaded_image)
 
-            # Perform the processing as before
-            data = [np.array(shap_values)]
-            image_array = data[0]
-            positive = np.where(image_array >= 0, image_array, 0)
-            negative = np.where(image_array < 0, image_array, 0)
-            negative_aps = np.abs(negative) 
+        # Perform the processing as before
+        data = [np.array(shap_values)]
+        image_array = data[0]
+        positive = np.where(image_array >= 0, image_array, 0)
+        negative = np.where(image_array < 0, image_array, 0)
+        negative_aps = np.abs(negative) 
 
-            flattened_array_pos = positive.flatten()
-            flattened_array_neg = negative_aps.flatten()
+        flattened_array_pos = positive.flatten()
+        flattened_array_neg = negative_aps.flatten()
 
-            normalized_array_pos = (flattened_array_pos - np.min(flattened_array_pos)) / (np.max(flattened_array_pos) - np.min(flattened_array_pos))
-            normalized_array_neg = (flattened_array_neg - np.min(flattened_array_neg)) / (np.max(flattened_array_neg) - np.min(flattened_array_neg))
+        normalized_array_pos = (flattened_array_pos - np.min(flattened_array_pos)) / (np.max(flattened_array_pos) - np.min(flattened_array_pos))
+        normalized_array_neg = (flattened_array_neg - np.min(flattened_array_neg)) / (np.max(flattened_array_neg) - np.min(flattened_array_neg))
 
-            normalized_positive = normalized_array_pos.reshape(positive.shape)
-            normalized_neg = normalized_array_neg.reshape(negative_aps.shape)
+        normalized_positive = normalized_array_pos.reshape(positive.shape)
+        normalized_neg = normalized_array_neg.reshape(negative_aps.shape)
 
-            grayscale_image_pos = normalized_positive / 3.0
-            grayscale_image_neg = normalized_neg / 3.0
-            print(grayscale_image_pos.shape)
-            grayscale_image_positive = np.mean(grayscale_image_pos, axis=-1)
-            grayscale_image_negative = np.mean(grayscale_image_neg, axis=-1)
-            print(grayscale_image_positive.shape)
+        grayscale_image_pos = normalized_positive / 3.0
+        grayscale_image_neg = normalized_neg / 3.0
+        print(grayscale_image_pos.shape)
+        grayscale_image_positive = np.mean(grayscale_image_pos, axis=-1)
+        grayscale_image_negative = np.mean(grayscale_image_neg, axis=-1)
+        print(grayscale_image_positive.shape)
 
-            grayscale_image_p = grayscale_image_positive.squeeze()
-            grayscale_image_n = grayscale_image_negative.squeeze()
+        grayscale_image_positive = grayscale_image_positive.squeeze()
+        grayscale_image_negative = grayscale_image_negative.squeeze()
 
-            percentile_95_pos = np.percentile(grayscale_image_p, 95)
-            percentile_95_neg = np.percentile(grayscale_image_n, 95)
+        percentile_95_pos = np.percentile(grayscale_image_positive, 95)
+        percentile_95_neg = np.percentile(grayscale_image_negative, 95)
 
-            grayscale_pos_thresholded = grayscale_image_p
-            grayscale_neg_thresholded = grayscale_image_n
+        grayscale_pos_thresholded = grayscale_image_positive
+        grayscale_neg_thresholded = grayscale_image_negative
 
-            grayscale_pos_thresholded[grayscale_pos_thresholded < percentile_95_pos] = 0
-            grayscale_neg_thresholded[grayscale_neg_thresholded < percentile_95_neg] = 0
+        grayscale_pos_thresholded[grayscale_pos_thresholded < percentile_95_pos] = 0
+        grayscale_neg_thresholded[grayscale_neg_thresholded < percentile_95_neg] = 0
 
 
         df_yolo_results = detect_image(img)  # Make sure 'detect' returns a DataFrame with YOLO detection results
